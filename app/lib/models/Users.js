24e7b85f3b3cc147d588e615195efc91dc06410f
Steve Campos <campos.coder@gmail.com>
2016-01-19 16:26:17 -0800

    includes Users collection helper.

---
+++ app/lib/models/Users.js
@@ -0,0 +1,16 @@
+// Users = Meteor.users ? hmm.
+
+// Meteor.users.allow({
+//   insert: () => false,
+//   update: () => false,
+//   remove: () => false
+// });
+//
+// Meteor.users.deny({
+//   insert: () => true,
+//   update: () => true,
+//   remove: () => true
+// });
+
+Meteor.users.helpers({
+});
